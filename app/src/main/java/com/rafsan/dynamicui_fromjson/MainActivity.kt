7376cf987432e8a7fdedb83d74a0ecbe9244404a
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-12-21 20:10:21 +0600

    mainactivity read json

--- app/src/main/java/com/rafsan/dynamicui_fromjson/MainActivity.kt
+++ app/src/main/java/com/rafsan/dynamicui_fromjson/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.rafsan.dynamicui_fromjson
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import com.rafsan.dynamicui_fromjson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val json = readRawJson()
+
+        binding.buttonView.setOnClickListener {
+            //Generate form
+            if (json.isNotEmpty()) {
+                val intent = Intent(this, GenerateFormActivity::class.java)
+                intent.putExtra("value", json)
+                startActivity(intent)
+            }
+        }
+    }
+
+    fun readRawJson(): String {
+        val text = resources.openRawResource(R.raw.sample_json)
+            .bufferedReader().use { it.readText() }
+        val check = checkValidity(text)
+        if (check) {
+            binding.etContent.setText(text)
+        } else {
+            Snackbar.make(binding.content, "Invalid JSON", Snackbar.LENGTH_SHORT).show()
+            return ""
+        }
+        return text
+    }
+
+    fun checkValidity(string: String): Boolean {
+        val gson = Gson()
+        try {
+            gson.fromJson(string, Object::class.java)
+        } catch (e: Exception) {
+            return false
+        }
+        return true
     }
 }
