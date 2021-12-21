163f2ce02e9f03bbc61ebbf63efecd35bd2c242e
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-12-22 01:24:26 +0600

    added helper methods

--- app/src/main/java/com/rafsan/dynamicui_fromjson/GenerateFormActivity.kt
+++ app/src/main/java/com/rafsan/dynamicui_fromjson/GenerateFormActivity.kt
@@ -1,7 +1,14 @@
 package com.rafsan.dynamicui_fromjson
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.InputType
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.MenuItem
@@ -142,6 +149,94 @@ class GenerateFormActivity : AppCompatActivity() {
         if (viewComponentModel.value != null) {
             view.text = Utils.fromHtml(viewComponentModel.value)
         }
+    }
+
+    private fun createLabelForViews(viewComponentModel: FormComponentItem) {
+        val label = TextView(this)
+        label.setTextColor(Color.BLACK)
+        label.setTypeface(null, Typeface.BOLD)
+        Utils.setMerginToviews(
+            label,
+            40,
+            LinearLayoutCompat.LayoutParams.WRAP_CONTENT,
+            LinearLayoutCompat.LayoutParams.WRAP_CONTENT
+        )
+        viewComponentModel.label?.let { labelText ->
+            viewComponentModel.required?.let {
+                if (it) {
+                    label.text = createStringForViewLabel(it, labelText)
+                } else {
+                    label.setText(createStringForViewLabel(false, labelText))
+                }
+            }
+            binding.miniAppFormContainer.addView(label)
+        }
+    }
+
+    /**
+     * EditText Input type selection
+     */
+    private fun setInputTypeForEditText(
+        editText: EditText,
+        viewComponentModel: FormComponentItem
+    ) {
+        when (viewComponentModel.subtype) {
+            "password" -> editText.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+            "email" -> editText.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            "tel" -> editText.inputType = InputType.TYPE_CLASS_PHONE
+            "dateTimeLocal" -> editText.inputType = InputType.TYPE_CLASS_DATETIME
+            else -> editText.inputType = InputType.TYPE_CLASS_TEXT
+        }
+    }
+
+    private fun createStringForViewLabel(
+        required: Boolean,
+        label: String
+    ): SpannableStringBuilder? {
+        val labelStr = Utils.fromHtml(label)
+        return if (required) {
+            labelStringForRequiredField(labelStr)
+        } else {
+            val username = SpannableString(labelStr)
+            val description = SpannableStringBuilder()
+            username.setSpan(
+                RelativeSizeSpan(1.1f), 0, username.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            username.setSpan(
+                ForegroundColorSpan(Color.parseColor("#000000")), 0, username.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            description.append(username)
+            description
+        }
+    }
+
+    private fun labelStringForRequiredField(label: String): SpannableStringBuilder? {
+        val username = SpannableString(label)
+        val description = SpannableStringBuilder()
+        username.setSpan(
+            RelativeSizeSpan(1.1f), 0, username.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        username.setSpan(
+            ForegroundColorSpan(Color.parseColor("#000000")), 0, username.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        description.append(username)
+        val commentSpannable = SpannableString(" *")
+        commentSpannable.setSpan(
+            ForegroundColorSpan(Color.RED), 0,
+            commentSpannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        commentSpannable.setSpan(
+            RelativeSizeSpan(1.0f), 0,
+            commentSpannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        description.append(commentSpannable)
+        return description
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/rafsan/dynamicui_fromjson/utils/Utils.kt
+++ app/src/main/java/com/rafsan/dynamicui_fromjson/utils/Utils.kt
@@ -2,12 +2,20 @@ package com.rafsan.dynamicui_fromjson.utils
 
 import android.os.Build
 import android.text.Html
+import android.view.View
+import android.widget.LinearLayout
 
 class Utils {
     companion object {
         fun fromHtml(str: String): String {
             return if (Build.VERSION.SDK_INT >= 24) Html.fromHtml(str, Html.FROM_HTML_MODE_LEGACY)
                 .toString() else Html.fromHtml(str).toString()
+        }
+
+        fun setMerginToviews(view: View, topMergin: Int, width: Int, height: Int) {
+            val layoutParams = LinearLayout.LayoutParams(width, height)
+            layoutParams.setMargins(40, topMergin, 40, 0)
+            view.layoutParams = layoutParams
         }
     }
 }
